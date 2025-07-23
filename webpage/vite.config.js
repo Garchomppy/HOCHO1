eb963748fe529e8b6b2283c3c758cd7e825684f5
Garchomppy <chontem123@gmail.com>
2025-07-23 13:49:41 +0700

    fix deploy navigation

--- webpage/vite.config.js
+++ webpage/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: mode === 'development' ? '/' : '/webpage/', // Tự động base cho local/dev vs build
   plugins: [react()],
   define: {
     global: 'window', // Polyfill để ánh xạ global thành window
